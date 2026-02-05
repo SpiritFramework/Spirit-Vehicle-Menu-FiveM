3217605f9c8b2525fdfe56361a2d9bed8a102bbd
SpiritFramework™ <95552895+SpiritFramework@users.noreply.github.com>
2026-02-05 16:14:59 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'Custom Styled Vehicle Spawner with NUI'
-author 'YourName'
+author 'SpiritFramework'
 version '2.0.0'
 
 ui_page 'html/index.html'
@@ -11,6 +11,10 @@ files {
     'html/index.html',
     'html/style.css',
     'html/script.js',
+}
+
+server_scripts {
+    'server.lua'
 }
 
 client_scripts {
